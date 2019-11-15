a5e224cc0ea8000c6bfa6d53a48a5a88afe79b37
tomsuzuki_macbook <b1017055@fun.ac.jp>
2019-11-15 14:10:38 +0900

    プレイリストの表示をJSから行えるようにした

--- html/js/index.js
+++ html/js/index.js
@@ -3,25 +3,47 @@
 
 // 現在再生中の曲を問い合わせる（他のデータもjsonで欲しいな...）
 window.addEventListener("DOMContentLoaded", function () {
-	
+	display_Playlist(["データ1","データ2"]);
 });
 
 // 曲を変えるための関数（引数は仮のもの）
-function control_Music(n){
-	if(n == 1); // 次の曲
-	if(n == -1); // リスタート
-	if(n == 0);	// 停止
+function control_Music(n) {
+	if (n == 1); // 次の曲
+	if (n == -1); // リスタート
+	if (n == 0);	// 停止
 }
 
 // 音量を切り替える
-function control_Volume(n){
-	if(n == -1); // 音量を下げる
-	if(n == 1); // 音量をあげる
+function control_Volume(n) {
+	if (n == -1); // 音量を下げる
+	if (n == 1); // 音量をあげる
 }
 
 // プレイリスト一覧を描画する関数
-function display_Playlist(s){
-	
+function display_Playlist(s) {
+	// PlayList--Frameのなかにリストを入れる
+	e = document.getElementById("PlayList--Frame");
+	while (e.firstChild) e.removeChild(e.firstChild);
+	if (s == null || s == [""]) {
+		var li = document.createElement("li");
+		li.textContent = "プレイリストが存在しません。";
+		e.appendChild(li);
+	} else {
+		for(var i in s) {
+			var li = document.createElement("li");
+			var a = document.createElement("a");
+			a.textContent = s[i];
+			a.setAttribute("href", "javascript:void(0);");
+			a.setAttribute("onclick", `load_Playlsit("${s[i]}")`);
+			li.appendChild(a);
+			e.appendChild(li);
+		}
+	}
+}
+
+// プレイリストをロードする
+function load_Playlsit(s){
+
 }
 
 /*
